3d3fdc5d11d4d112b47d2371b251ab900a26b4dd
Rafael Silva <rafaelsilvar74@gmail.com>
2026-05-28 15:37:02 -0300

    feat: alterar configuração do tema para modo claro e ajustar lógica de inicialização

--- quasar.config.js
+++ quasar.config.js
@@ -95,7 +95,7 @@ export default defineConfig((ctx) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: { dark: true },
+      config: { dark: false },
 
       iconSet: 'fontawesome-v6',
       lang: 'pt-BR', // Quasar language pack
--- src/boot/theme.js
+++ src/boot/theme.js
@@ -4,5 +4,7 @@ export default () => {
   const saved = localStorage.getItem('theme')
   if (saved !== null) {
     Dark.set(saved === 'dark')
+  } else {
+    Dark.set(false)
   }
 }
